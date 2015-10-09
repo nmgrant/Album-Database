0661991cf1a7c479b7ffece87c5d4afac13a2327
Nicholas Grant <ngrant40@gmail.com>
2015-10-08 23:57:11 -0700

    Added list album data functionality

--- src/DatabaseAccess.java
+++ src/DatabaseAccess.java
@@ -13,6 +13,7 @@ public class DatabaseAccess { // JDBC driver name and database URL
     static final String PASS = "newpass";
 
     private Connection conn;
+    private Statement stmt;
 
     public DatabaseAccess() throws Exception {
         Class.forName(JDBC_DRIVER);
@@ -27,12 +28,28 @@ public class DatabaseAccess { // JDBC driver name and database URL
             conn.close();
         } catch (SQLException se) {
             se.printStackTrace();
+        } finally {
+            try {
+                if (stmt != null) {
+                    stmt.close();
+                }
+            } catch (SQLException se2) {
+            }
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+            System.out.println("Goodbye!");
         }
     }
     
     public ArrayList<String> listAlbumTitles() {
-        Statement stmt = null;
+        stmt = null;
         ArrayList<String> albumTitles = new ArrayList<String>();
+        albumTitles.add("Album Titles");
         try {
             stmt = conn.createStatement();
             String sql;
@@ -49,6 +66,50 @@ public class DatabaseAccess { // JDBC driver name and database URL
         }
         if (!albumTitles.isEmpty()) {
             return albumTitles;
+        }
+        else {
+            return null;
+        }
+    }
+    
+    public ArrayList<String> listAlbumData(String album) {
+        Statement stmt = null;
+        ArrayList<String> data = new ArrayList<String>();
+        try {
+            stmt = conn.createStatement();
+            String sql;
+            sql = "SELECT * FROM album WHERE album_title = '" + album + "'";
+            ResultSet rs = stmt.executeQuery(sql);
+            
+            while (rs.next()) {
+                String albumTitle = rs.getString("album_title");
+                String groupName = rs.getString("group_name");
+                String studioName = rs.getString("studio_name");
+                String dateRecorded = rs.getString("date_recorded");
+                String length = rs.getString("length");
+                String numberOfSongs = rs.getString("number_of_songs");
+                
+                /*data.add("Title: " + albumTitle);
+                data.add("Group name: " + groupName);
+                data.add("Studio name: " + studioName);
+                data.add("Date recorded: " + dateRecorded);
+                data.add("Length: " + length);
+                data.add("Number of Songs: " + numberOfSongs);*/
+                
+                data.add(albumTitle);
+                data.add(groupName);
+                data.add(studioName);
+                data.add(dateRecorded);
+                data.add(length);
+                data.add(numberOfSongs);
+            }
+            rs.close();
+            stmt.close();
+        } catch (SQLException se) {
+            se.printStackTrace();
+        }
+        if (!data.isEmpty()) {
+            return data;
         }
         else {
             return null;
