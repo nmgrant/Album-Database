868369353cbd9c90af861445c13b2856049a6728
012437761 <012437761@ecs412pc86.campus.ad.csulb.edu>
2015-10-14 16:07:17 -0700

    Added editing functionality

--- src/AlbumTableModel.java
+++ src/AlbumTableModel.java
@@ -1,5 +1,6 @@
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import javax.swing.table.AbstractTableModel;
 
 public class AlbumTableModel extends AbstractTableModel {
@@ -10,35 +11,45 @@ public class AlbumTableModel extends AbstractTableModel {
     private final int DATE_RECORDED = 3;
     private final int LENGTH = 4;
     private final int NUMBER_OF_SONGS = 5;
-    
-    private String[] columnNames = { "Title", "Group", "Studio",
-     "Date", "Length", "# of Songs"
+
+    private String[] columnNames = {"Title", "Group", "Studio",
+        "Date", "Length", "# of Songs"
     };
-    
+
     private ArrayList<String> albums;
-    
+    private Object[][] data = { {"Title", "Group", "", "", "", ""} };
+
     public AlbumTableModel(ArrayList<String> album) {
         albums = album;
     }
-    
+
     @Override
     public String getColumnName(int col) {
         return columnNames[col];
     }
-    
+
     @Override
     public int getColumnCount() {
         return columnNames.length;
     }
-    
+
     @Override
     public int getRowCount() {
         return 1;
     }
-    
+
+    public boolean isCellEditable(int row, int col) {
+        return true;
+    }
+
+    public void setValueAt(String value, int row, int col) {
+        data[row][col] = value;
+        fireTableCellUpdated(row, col);
+    }
+
     @Override
     public String getValueAt(int row, int col) {
-        
+
         switch (col) {
             case ALBUM_TITLE:
                 return albums.get(ALBUM_TITLE);
