e086677d9bf76de175555292c666d67a4ff8bc78
Nicholas Grant <ngrant40@gmail.com>
2015-10-15 00:23:47 -0700

    Adjusted cell editing

--- src/AlbumTableModel.java
+++ src/AlbumTableModel.java
@@ -2,6 +2,7 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import javax.swing.table.AbstractTableModel;
+import javax.swing.table.DefaultTableModel;
 
 public class AlbumTableModel extends AbstractTableModel {
 
@@ -15,19 +16,27 @@ public class AlbumTableModel extends AbstractTableModel {
     private String[] columnNames = {"Title", "Group", "Studio",
         "Date", "Length", "# of Songs"
     };
+    private boolean[] editableCells = { true, true, true, true, true, true };
 
     private ArrayList<String> albums;
-    private Object[][] data = { {"Title", "Group", "", "", "", ""} };
 
     public AlbumTableModel(ArrayList<String> album) {
-        albums = album;
+        this.albums = album;
+        for (int i = 0; i < 6; i++) {
+            setValueAt(album.get(i), 0, i);
+        }
+    }
+
+    @Override
+    public Class getColumnClass(int c) {
+        return getValueAt(0, c).getClass();
     }
 
     @Override
     public String getColumnName(int col) {
         return columnNames[col];
     }
-
+    
     @Override
     public int getColumnCount() {
         return columnNames.length;
@@ -38,33 +47,28 @@ public class AlbumTableModel extends AbstractTableModel {
         return 1;
     }
 
+    @Override
     public boolean isCellEditable(int row, int col) {
-        return true;
+        return editableCells[col];
     }
-
-    public void setValueAt(String value, int row, int col) {
-        data[row][col] = value;
-        fireTableCellUpdated(row, col);
+    
+    public void setCellsEditable(boolean value) {
+        for (int i = 0; i < editableCells.length; i++)
+            editableCells[i] = value;
     }
 
     @Override
-    public String getValueAt(int row, int col) {
+    public void setValueAt(Object value, int row, int col) {
 
-        switch (col) {
-            case ALBUM_TITLE:
-                return albums.get(ALBUM_TITLE);
-            case GROUP_NAME:
-                return albums.get(GROUP_NAME);
-            case STUDIO_NAME:
-                return albums.get(STUDIO_NAME);
-            case DATE_RECORDED:
-                return albums.get(DATE_RECORDED);
-            case LENGTH:
-                return albums.get(LENGTH);
-            case NUMBER_OF_SONGS:
-                return albums.get(NUMBER_OF_SONGS);
-            default:
-                return albums.get(ALBUM_TITLE);
-        }
+        albums.set(col, value.toString());
+        fireTableCellUpdated(row, col);
+
     }
+
+    @Override
+    public Object getValueAt(int row, int col) {
+        return albums.get(col);
+    }
+
+    
 }
